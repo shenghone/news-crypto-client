edcbe11edb97ea84d9897013422fd474ce2c2a24
shenghone <shenghone@gmail.com>
2020-04-29 18:15:13 -0400

    add dotenv

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddelware from "redux-saga";
 import rootReducer from "./reducers";
 import rootSaga from "../saga";
+import { config } from "dotenv";
+config();
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddelware();
